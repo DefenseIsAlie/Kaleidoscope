1761139102a897c4c3008a083dd563580aa1b221
Abhishek Josyula <91714711+DefenseIsAlie@users.noreply.github.com>
2023-08-06 22:48:13 +0530

    Working Pipeline with maven antlr a simple rule

--- my-first-frontend/src/main/java/abhishek/josyula/App.java
+++ my-first-frontend/src/main/java/abhishek/josyula/App.java
@@ -1,4 +1,10 @@
 package abhishek.josyula;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
 /**
  * Hello world!
  *
@@ -6,7 +12,32 @@ package abhishek.josyula;
 public class App 
 {
     public static void main( String[] args )
-    {
+    {   
         
+        try {
+            ExprLexer lex = new ExprLexer(CharStreams
+            .fromString("10-3*5"));
+
+            CommonTokenStream strm = new CommonTokenStream(lex);
+            ExprParser pr = new ExprParser(strm);
+
+            ParseTree tr = pr.expr();
+            ParseTreeWalker wlkr = new ParseTreeWalker();
+            T4 listner = new T4();
+            
+            wlkr.walk(listner, tr);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
+class T4 extends ExprBaseListener {
+    @Override
+    public void enterEveryRule(ParserRuleContext ctx){
+        System.out.println("Hello World");
+        System.out.println(ctx.getText());
     }
 }
