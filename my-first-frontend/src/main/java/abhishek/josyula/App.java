d0f64cb26fb11b61b940dbe3601bd5c7e6bee67f
Abhishek Josyula <91714711+DefenseIsAlie@users.noreply.github.com>
2023-12-22 12:33:41 +0530

    moved Toy lang to front

--- my-first-frontend/src/main/java/abhishek/josyula/App.java
+++ my-first-frontend/src/main/java/abhishek/josyula/App.java
@@ -5,6 +5,8 @@ import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
+import abhishek.josyula.ExprParser.ProgContext;
+
 /**
  * Hello world!
  *
@@ -13,10 +15,9 @@ public class App
 {
     public static void main( String[] args )
     {   
-        
         try {
             ExprLexer lex = new ExprLexer(CharStreams
-            .fromString("10-3*5"));
+                    .fromString("10-3*5"));
 
             CommonTokenStream strm = new CommonTokenStream(lex);
             ExprParser pr = new ExprParser(strm);
@@ -24,7 +25,7 @@ public class App
             ParseTree tr = pr.expr();
             ParseTreeWalker wlkr = new ParseTreeWalker();
             T4 listner = new T4();
-            
+
             wlkr.walk(listner, tr);
 
         } catch (Exception e) {
@@ -36,8 +37,12 @@ public class App
 
 class T4 extends ExprBaseListener {
     @Override
-    public void enterEveryRule(ParserRuleContext ctx){
+    public void exitProg(ProgContext ctx) {
         System.out.println("Hello World");
+    }
+
+    @Override
+    public void enterEveryRule(ParserRuleContext ctx){
         System.out.println(ctx.getText());
     }
 }
